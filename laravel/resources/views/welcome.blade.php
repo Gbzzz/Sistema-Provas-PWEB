a12540414f88dc705d44205852cea5155e45da84
Pedro Henrique dos Santos <pedro110302@gmail.com>
2023-04-04 00:03:22 -0300

    o rei do javascript

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -4,29 +4,83 @@
 
 @section('content')
 
-<h1>Prova Virtual</h1>
-<h2>Formulário para Criação de Prova Online</h2>
+<body>
 
-    <form action="{{ url('/validar') }}" method="POST">
-        @csrf
-         
-        <table>
-            <tr>
-                <th><h3>Questões:</h3></th>
-            </tr>
-            <td>
-                <div id="atividadesExtra">
-                    <h4>Questão 1</h4>
-                    <input type="checkbox" class="check" name="atividade[]" value="informatica">Aberta
-                    <input type="checkbox" class="check" name="atividade[]" value="musica">Múltiplas escolhas (1 correta)
-                    <input type="checkbox" class="check" name="atividade[]" value="balet">Múltiplas escolhas (+ de 1 correta)
-                    <input type="checkbox" class="check" name="atividade[]" value="pintura">V ou F
-                </div>
-            </td>
-        </table>
-        <div id="botoes">
-            <button type="submit" class="button-enviar">Criar Prova</button>
-            <button href="{{ url('/') }}" type="reset" class="button-limpar">Limpar Campos</button>
-        </div>
+<form>
+<h3>QUESTÃO 1</h3>
+
+<label>Enunciado:</label> <input type="text"> <br>
+
+<label>Tipo de questão:</label>
+<select id="tipos" onchange="escolherQuestao()">
+    <option disabled selected value> -- Escolha uma opção --</option>
+    <option value="1">Aberta</option>
+    <option value="2">Múltipla escolha (1 correta)</option>
+    <option value="3">Múltipla escolha (mais de 1 correta)</option>
+    <option value="4">Verdadeiro ou falso</option>
+</select>
+
+<br>
+<br>
+
+<div id="questao-aberta" style="display:none;"><input type="text"> </div>
+
+<div id="questao-fechada-1" style="display:none">
+    <input type="radio" name="opcao" value ="1"> Opcão 1<br>
+    <input type="radio" name="opcao" value ="2"> Opção 2<br>
+    <input type="radio" name="opcao" value ="3"> Opção 3<br>
+    <input type="radio" name="opcao" value ="4"> Opção 4
+</div>
+
+<div id="questao-fechada-2" style="display:none">
+    <input type="checkbox" value ="1"> Opcão 1<br>
+    <input type="checkbox" value ="2"> Opção 2<br>
+    <input type="checkbox" value ="3"> Opção 3<br>
+    <input type="checkbox" value ="4"> Opção 4
+</div>
+
+
+</form>
+    
+<script>
+
+function escolherQuestao() {
+
+    var select = document.getElementById("tipos");
+    var valor = select.options[select.selectedIndex].value;
+    if (valor == 1) {
+
+        var fechada1 = document.getElementById("questao-fechada-1");
+        fechada1.style.display = 'none';
+        var fechada2 = document.getElementById("questao-fechada-2");
+        fechada2.style.display = 'none';
+        var aberta = document.getElementById("questao-aberta");
+        aberta.style.display = 'block';
+
+    } else if (valor == 2) {
+
+        var fechada1 = document.getElementById("questao-fechada-1");
+        fechada1.style.display = 'block';
+        var fechada2 = document.getElementById("questao-fechada-2");
+        fechada2.style.display = 'none';
+        var aberta = document.getElementById("questao-aberta");
+        aberta.style.display = 'none';
+
+    } else if ( valor == 3) {
+
+        var fechada1 = document.getElementById("questao-fechada-1");
+        fechada1.style.display = 'none';
+        var fechada2 = document.getElementById("questao-fechada-2");
+        fechada2.style.display = 'block';
+        var aberta = document.getElementById("questao-aberta");
+        aberta.style.display = 'none';
+
+    } else {
+        document.write("Você escolheu: questão de verdadeiro ou falso.")
+    }
+}
+
+</script>
+</body>
 
 @endsection
