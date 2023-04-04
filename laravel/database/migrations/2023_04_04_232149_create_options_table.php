501b2d5b4aeec72f32932701e3b7283db15322df
Pedro Henrique dos Santos <pedro110302@gmail.com>
2023-04-04 20:23:30 -0300

    criação das migrations

---
+++ laravel/database/migrations/2023_04_04_232149_create_options_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('options', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_questao', false);
+            $table->foreign('id_questao')->references('id_questao')->on('questions');
+            $table->text('enunciado');
+            $table->boolean('correcao');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('options');
+    }
+}
