cefd12e156c68d83c55db8414c25c538ac52606a
Gbzzz <gbp3@aluno.ifal.edu.br>
2023-04-11 02:43:12 +0000

    add modal's

--- laravel/resources/views/questions/index.blade.php
+++ laravel/resources/views/questions/index.blade.php
@@ -10,20 +10,112 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <div class="border border-gray-200 px-4 py-2">
-                        <form action="{{ route('add_question') }}" method="POST">
-                            @csrf
-                            <div class="mt-4">
-                                <x-label for="text" :value="__('Pergunta')" />
 
-                                <x-input id="text" class="block mt-1 w-full" type="text" name="description" :value="old('description')" required />
+                        <x-label for="text" :value="__('Criar Questão')" />
+                        <x-button type="button" class="mt" data-toggle="modal" data-target="#aberta-Modal">
+                            Questão Aberta
+                        </x-button>
+
+                        <x-button type="button" class="mt" data-toggle="modal" data-target="#v-f-Modal">
+                            Questão de V/F
+                        </x-button>
+                        
+                        <x-button type="button" class="mt" data-toggle="modal" data-target="#multipla-escolha-Modal">
+                            Questão Multipla Escolha
+                        </x-button>
+
+                        <x-button type="button" class="mt" data-toggle="modal" data-target="#uma-correta-Modal">
+                            Questão com Apenas 1 Correta
+                        </x-button>
+                        
+                        <!-- Modal Aberta -->
+                        <div class="modal fade" id="aberta-Modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Questão Aberta</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                                </div>
+                                <div class="modal-body">
+                                ...
+                                </div>
+                                <div class="modal-footer">
+                                    <x-button type="button" class="mt" data-dismiss="modal">Close</x-button>
+                                    <x-button type="button" class="mt">Save changes</x-button>
+                                </div>
                             </div>
-                            <x-button class="mt-4">
-                                {{ __('Registrar') }}
-                            </x-button>
-                        </form>
+                            </div>
+                        </div>
+
+                        <!-- Modal V/F -->
+                        <div class="modal fade" id="v-f-Modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Questão de V/F</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                                </div>
+                                <div class="modal-body">
+                                ...
+                                </div>
+                                <div class="modal-footer">
+                                    <x-button type="button" class="mt" data-dismiss="modal">Close</x-button>
+                                    <x-button type="button" class="mt">Save changes</x-button>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+
+                        <!-- Modal Multipla Escolha -->
+                        <div class="modal fade" id="multipla-escolha-Modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Questão de Multipla Escolha</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                                </div>
+                                <div class="modal-body">
+                                ...
+                                </div>
+                                <div class="modal-footer">
+                                    <x-button type="button" class="mt" data-dismiss="modal">Close</x-button>
+                                    <x-button type="button" class="mt">Save changes</x-button>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+
+                        <!-- Modal com Apenas 1 Correta -->
+                        <div class="modal fade" id="uma-correta-Modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Questão com apenas 1 correta</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                                </div>
+                                <div class="modal-body">
+                                ...
+                                </div>
+                                <div class="modal-footer">
+                                    <x-button type="button" class="mt" data-dismiss="modal">Close</x-button>
+                                    <x-button type="button" class="mt">Save changes</x-button>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
+
 </x-app-layout>
